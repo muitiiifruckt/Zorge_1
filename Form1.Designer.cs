9fedc3e9a4a2b697e793ec6e5afeb1c05349cc49
multifrukt <multifrukt@MULTIFRUKT>
2023-03-08 10:13:01 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Zorge_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.encrypt_btn = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(21, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(398, 151);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(477, 23);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(398, 151);
+            this.richTextBox2.TabIndex = 0;
+            this.richTextBox2.Text = "";
+            // 
+            // encrypt_btn
+            // 
+            this.encrypt_btn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.encrypt_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.encrypt_btn.Location = new System.Drawing.Point(21, 309);
+            this.encrypt_btn.Name = "encrypt_btn";
+            this.encrypt_btn.Size = new System.Drawing.Size(108, 32);
+            this.encrypt_btn.TabIndex = 1;
+            this.encrypt_btn.Text = "Зашифровать ";
+            this.encrypt_btn.UseVisualStyleBackColor = false;
+            this.encrypt_btn.Click += new System.EventHandler(this.encrypt_btn_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(21, 199);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(175, 37);
+            this.richTextBox3.TabIndex = 0;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(21, 255);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(175, 37);
+            this.richTextBox4.TabIndex = 0;
+            this.richTextBox4.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ключ из 6 букв ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(304, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Анаграмма из наиболее встречающихся букв \"a s i n t o e r\" ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 518);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.encrypt_btn);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button encrypt_btn;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zorge_1
+{
+    public partial class Form1 : Form
+    {
+        string S = "";
+        string alf = "abcdefghijklmnopqrstuvwxyz";
+        string numbers = "0123456789";
+        string alf_numbers = "abcdefghijklmnopqrstuvwxyz0123456789"; 
+        string text = "";
+        string text_1 = "";
+        string key = "";
+        string anagramma = "";
+        char[,] tabl_1 = new char[5, 6];
+        char[,] tabl_3 = new char[3, 10];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void encrypt(string key, string anagramma)
+        {
+            text_1 = richTextBox1.Text.ToLower();//
+            foreach( char p in text_1)//очитска от лишних символов
+            {
+                if (string_in(p, alf_numbers))
+                    text += p;
+            }
+
+            // проверки на правильность 
+            try
+            {
+                if (key.Length != 6 && anagramma.Length != 8) throw new Exception("Длина ключа должна быть 6, длина анаграммы должна быть 8 и должна включать самые популярные буквы алфавита \"asintoer\"");
+                
+
+                //
+                for (int i = 0; i < key.Length; i++)
+                    if (!string_in(key[i], alf)) throw new Exception("В ключе присутствует лишние символы ( другой язык, пробелы, знаки препинания, верхний регистр и т.д.)");
+                //
+                for (int i = 0; i < anagramma.Length; i++)
+                    if (!string_in(anagramma[i], alf)) throw new Exception("В анаграмме присутствует лишние символы ( другой язык, пробелы, знаки препинания, верхний регистр и т.д.)");
+
+
+                //
+                for (int i =0;i<key.Length; i++)
+                    for(int j = 1;j>i && j <key.Length;j++)
+                        if (key[i] == key[j])
+                            throw new Exception("В ключе все буквы должны быть разные");
+
+
+                //
+                for (int i = 0; i < anagramma.Length; i++)
+                    for (int j = 1; j > i && j < key.Length; j++)
+                        if (anagramma[i] == anagramma[j])
+                            throw new Exception("В анаграмме все буквы должны быть разные");
+
+                //
+            }
+            catch (Exception e)
+            { MessageBox.Show($"Ошибка: {e.Message}"); }
+
+            //
+            create_table();
+            //
+            bool is_first_number = true;
+            bool Isnumber = false;
+            //
+            foreach (char k in text)
+            {
+
+                if (string_in(k, numbers)) // определяем цифра  ли это
+                  Isnumber = true; 
+                else 
+                    Isnumber = false;
+
+                /////
+                ///
+
+
+                if(Isnumber ) // если это число
+                {
+                    if(is_first_number)
+                    {
+                        S += "94" + k + k;
+                        is_first_number = false;
+                    }
+                    else
+                    {
+                        S +=  k ;
+                        S += k ;
+                    }
+                }
+                else// если это  НЕ число
+                {
+                    if (!(S[S.Length-2]=='9' && S[S.Length - 1] == '4') && !is_first_number)
+                    {
+                        S += "94";
+                    }
+                    is_first_number = true;
+                    int i = 0;
+                    int j = 0;
+
+                    for(;i<3;i++)// поиск индекосв в конечной таблице 
+                    {
+                        j = 0;
+                        for (; j < 10; j++)
+                        {
+                            if (k == tabl_3[i,j])
+                                goto found;
+                        }
+                    }
+                found:
+                    if (i == 0)
+                        S += j;
+                    else
+                    {
+                        S += (i+7) ;
+                        S +=  j;
+                    }
+
+                }
+            }
+            if (string_in(text[text.Length-1],numbers)) // если последний символ был цифрой 
+                S += "94";
+
+            richTextBox2.Text= S;
+        }
+        
+        private void create_table()
+        {
+            // заполнение первой таблимцы 
+            for (int i = 0; i < 6; i++) // первая строка просто ключевое слово
+            {
+                tabl_1[0, i] = key[i];
+            }
+            ////
+            ///
+
+            int k = 0;
+            for (int i = 1; i < 5; i++) // заполнение 
+            {
+                for (int j = 0; j < 6 && i * 6 + j < alf.Length; j++)
+                {
+                    for (; k < alf.Length; k++)
+                    {
+
+                        if (!string_in(alf[k], key))
+                        { break; }
+
+                    }
+                    tabl_1[i, j] = alf[k];
+                    k++;
+                }
+            }
+
+            ////
+            ///
+
+            tabl_1[4, 2] = '.';
+            tabl_1[4, 3] = '/';
+            // заполнение первой таблимцы 
+
+            // заполнение 3 таблимцы 
+            tabl_3[2, 0] = '.';
+            tabl_3[2, 0] = '/';
+            int p = 0; // счетчик для заполнения первой строки 
+            int q = 0; //  счетчик для последовательного заполнения таблицы по строкам ( второй и третьей)
+
+            ////
+            ///
+
+            for (int i = 0; i < 6; i++) // заполнение конечной таблицы
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (string_in(tabl_1[j, i], anagramma)) // заполнение первой строки буквами из анаграммы по очередности появления в в таблице 1 ( от столбцов к строкам) 
+                    {
+                        tabl_3[0, p] = tabl_1[j, i];
+                        p++;
+                    }
+                    else if (q == 20)
+                        break;
+                    else if (q < 10) // заполнение второй строки буквами из не анаграммы 
+                    {
+                        tabl_3[1, q] = tabl_1[j, i];
+                        q++;
+                    }
+                    else if (q >= 10 && !(tabl_3[1, q - 10] == '.' || tabl_3[1, q - 10] == '/')) // заполнение третьей строки буквами из не анаграммы
+                    {
+                        if (i == 4 && j == 4) // в таблице пусто и нужно просто перейти на след значение
+                            continue;
+                        else
+                        {
+                            tabl_3[2, q - 10] = tabl_1[j, i];
+                            q++;
+                        }
+                    }
+                    else // перевод хода
+                    {
+                        q++;
+                    }
+                }
+            }
+            // заполнение 3 таблимцы 
+        }
+
+        public bool string_in(char a, string s) // функция, которая проверяет наличие того или иного элемента в алфавите 
+        {
+            for (int i = 0; i < s.Length; i++)
+                if (a == s[i])
+                    return true;
+            return false;
+        }
+
+
+        private void encrypt_btn_Click(object sender, EventArgs e)
+        {
+            key = richTextBox3.Text;
+            anagramma = richTextBox4.Text;
+            encrypt(key, anagramma); // запуск
+        }
+    }
+}
